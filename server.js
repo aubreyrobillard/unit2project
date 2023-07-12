a2cc7514bf29255fc8f84f1769ed242d107b39db
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-07-11 20:53:29 -0500

    complete new and edit routes

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const methodOverride = require('method-override');
 const mongoose = require('./models/connection');
 const RestaurantRouter = require('./controllers/restaurantRoutes');
+const methodOverride = require('method-override');
 
 // EXPRESS APPLICATION
 const app = express();
@@ -13,6 +13,7 @@ const app = express();
 app.use(morgan('dev')); //logging
 app.use(methodOverride('_method')); //override with POST having ?_method=DELETE or ?_method=PUT
 app.use(express.static('public')); //serve static files from public folder
+app.use(express.urlencoded({extended:true})) //this is how you gain access to req.body
 
 app.use('/restaurants', RestaurantRouter);
 
